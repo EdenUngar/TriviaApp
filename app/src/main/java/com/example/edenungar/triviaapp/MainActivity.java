f119e3000a9ddce089e4859590c0b71923090dd7
Eden Ungar <edenungar@gmail.com>
2018-06-19 14:17:05 -0400

    implemented butterknife and binded ui elements and created a FrameLayout

--- app/src/main/java/com/example/edenungar/triviaapp/MainActivity.java
+++ app/src/main/java/com/example/edenungar/triviaapp/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.edenungar.triviaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private QuestionCreatorFragment questionCreatorFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //just put "this" because this is an activity and the fragment is not
+        ButterKnife.bind(this);
     }
+
+    //whatever you put in this method will be executed
+    @OnClick (R.id.add_question_button)
+    protected void addQuestionClicked(){
+
+        questionCreatorFragment = QuestionCreatorFragment.newInstance();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_holder, questionCreatorFragment).commit();
+
+    }
+
 }
